365b478b67271812b85e4dde049fc5968f09766f
raphaelsegalla <raphael.segalla@gmail.com>
2025-05-14 21:07:12 -0300

    Implementando a consulta de sensores

--- src/main/java/com/algaworks/algasensors/device/management/api/config/web/StringToTSIDWebConverter.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/config/web/StringToTSIDWebConverter.java
@@ -2,9 +2,7 @@ package com.algaworks.algasensors.device.management.api.config.web;
 
 import io.hypersistence.tsid.TSID;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.stereotype.Component;
 
-@Component
 public class StringToTSIDWebConverter implements Converter<String, TSID> {
     @Override
     public TSID convert(String source) {
